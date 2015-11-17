e06492291b158b5aef6eae921d360959ff280929
mukko <mukk0.aquarius@gmail.com>
2015-11-17 15:40:47 +0900

    曜日を求めるためにツェラーの公式を作成する。設定ファイルのメインをそれ用のものに変更

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "2.0.0"]]
-  :aot [sandbox.core]
-  :main sandbox.core)
+                 [clj-http "2.0.0"]
+                 [clj-time "0.11.0"]]
+  :aot [sandbox.zeller]
+  :main sandbox.zeller)
